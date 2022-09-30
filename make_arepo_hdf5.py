74c10d42c5d988d4e80bf44d74de5c6c6839eafe
Sean Lewis <slewis@int1.local.snellius.surf.nl>
2022-09-30 17:54:43 +0200

    matches AREPO output for (i think) all fields I need

--- make_arepo_hdf5.py
+++ make_arepo_hdf5.py
@@ -7,7 +7,9 @@ import h5py
 # HDF5 structure - arepo_test.hdf5/PartType0/Coordinates
 
 coords = np.zeros((4,3))
+vels = np.zeros((4,3))
 dens = np.zeros(4)
+eint = np.zeros(4)
 #for i in range(len(mass)):
 #    coords[i] = [-1.5, 1.5, 0.0]
 #    mass[i] = 10.0
@@ -16,16 +18,28 @@ coords[1] = [307.58379038431644, 296.06670264057715, 300.0282243435139]
 coords[2] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 coords[3] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 
+vels[0] = [100., 100., 100.]
+vels[1] = [100., 100., 100.]
+vels[2] = [-100., -100., -100.]
+vels[3] = [-100., -100., -100.]
+
 dens[0] = 1.0e-21
 dens[1] = 1.0e-21
 dens[2] = 1.0e-19
 dens[3] = 1.0e-19
-print(coords)
-print(coords.dtype)
-print(dens)
-print(dens.dtype)
+
+eint[0] = 1.0e-3
+eint[1] = 1.0e-3
+eint[2] = 1.0e-5
+eint[3] = 1.0e-5
+print(coords, coords.dtype)
+print(vels, vels.dtype)
+print(dens, dens.dtype)
+print(eint, eint.dtype)
 
 f = h5py.File('voramr_test.hdf5', 'w')
 group = f.create_group('PartType0')
 dset = group.create_dataset('Coordinates', data = coords, dtype = 'd')
+dset = group.create_dataset('Velocities', data = vels, dtype = 'd')
 dset = group.create_dataset('Density', data = dens, dtype = 'd')
+dset = group.create_dataset('InternalEnergy', data = eint, dtype = 'd')
