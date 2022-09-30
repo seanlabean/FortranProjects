76104466e5568fa7d09c1f037c4da88e134abb4f
Sean Lewis <slewis@int1.local.snellius.surf.nl>
2022-09-30 17:42:26 +0200

    want to access intrinsic values like density

--- make_arepo_hdf5.py
+++ make_arepo_hdf5.py
@@ -7,7 +7,7 @@ import h5py
 # HDF5 structure - arepo_test.hdf5/PartType0/Coordinates
 
 coords = np.zeros((4,3))
-mass = np.zeros(4)
+dens = np.zeros(4)
 #for i in range(len(mass)):
 #    coords[i] = [-1.5, 1.5, 0.0]
 #    mass[i] = 10.0
@@ -16,13 +16,16 @@ coords[1] = [307.58379038431644, 296.06670264057715, 300.0282243435139]
 coords[2] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 coords[3] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 
-mass[0] = 1.4285714285714285e-07
-mass[1] = 1.4285714285714285e-07
-mass[2] = 1.4285714285714285e-07
+dens[0] = 1.0e-21
+dens[1] = 1.0e-21
+dens[2] = 1.0e-19
+dens[3] = 1.0e-19
 print(coords)
 print(coords.dtype)
+print(dens)
+print(dens.dtype)
 
 f = h5py.File('voramr_test.hdf5', 'w')
 group = f.create_group('PartType0')
 dset = group.create_dataset('Coordinates', data = coords, dtype = 'd')
-dset = group.create_dataset('Masses', data = mass, dtype = 'd')
+dset = group.create_dataset('Density', data = dens, dtype = 'd')
