b3c8d02ad832a57c8ccb0dca7fd5504960dfb586
Sean Lewis <slewis@int4.local.snellius.surf.nl>
2023-03-01 18:14:55 +0100

    tracking changes for hdf5 maker

--- make_arepo_hdf5.py
+++ make_arepo_hdf5.py
@@ -8,6 +8,7 @@ import h5py
 
 coords = np.zeros((4,3))
 vels = np.zeros((4,3))
+mass = np.zeros(4)
 dens = np.zeros(4)
 eint = np.zeros(4)
 #for i in range(len(mass)):
@@ -18,22 +19,28 @@ coords[1] = [307.58379038431644, 296.06670264057715, 300.0282243435139]
 coords[2] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 coords[3] = [306.8695046700308,  295.3524169262915, 300.0282243435139]
 
-vels[0] = [100., 100., 100.]
-vels[1] = [100., 100., 100.]
-vels[2] = [-100., -100., -100.]
-vels[3] = [-100., -100., -100.]
+mass[0] = 3.0e-7
+mass[1] = 3.0e-7
+mass[2] = 4.0e-9
+mass[3] = 4.0e-9
+
+vels[0] = [500., 500., 500.]
+vels[1] = [500., 500., 500.]
+vels[2] = [-600., -600., -600.]
+vels[3] = [-600., -600., -600.]
 
 dens[0] = 1.0e-21
 dens[1] = 1.0e-21
-dens[2] = 1.0e-19
-dens[3] = 1.0e-19
+dens[2] = 2.0e-19
+dens[3] = 2.0e-19
 
-eint[0] = 1.0e-3
-eint[1] = 1.0e-3
-eint[2] = 1.0e-5
-eint[3] = 1.0e-5
+eint[0] = 7.0e-3
+eint[1] = 7.0e-3
+eint[2] = 8.0e-5
+eint[3] = 8.0e-5
 print(coords, coords.dtype)
 print(vels, vels.dtype)
+print(mass, mass.dtype)
 print(dens, dens.dtype)
 print(eint, eint.dtype)
 
@@ -41,5 +48,6 @@ f = h5py.File('voramr_test.hdf5', 'w')
 group = f.create_group('PartType0')
 dset = group.create_dataset('Coordinates', data = coords, dtype = 'd')
 dset = group.create_dataset('Velocities', data = vels, dtype = 'd')
+dset = group.create_dataset('Masses', data = mass, dtype = 'd')
 dset = group.create_dataset('Density', data = dens, dtype = 'd')
 dset = group.create_dataset('InternalEnergy', data = eint, dtype = 'd')
